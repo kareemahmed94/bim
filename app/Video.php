c830aa1c062eca628b72cbbcb1ef43b38ece8197
kareem <kareemahmed73@gmail.com>
2019-08-20 15:45:33 +0200

    Update Video.php

--- app/Video.php
+++ app/Video.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Video extends Model
 {
     protected $fillable=[
-        'name_ar','name_en','description_ar','description_en','course_id','path',
+        'name_ar','name_en','description_ar','description_en','course_id','path','test'
     ];
 
     public function course(){
